aa5ea261043c99536ddc4438abb6b89c02caac6d
Emran Talukdar <talukderemran61@gmail.com>
2026-01-19 21:25:57 +1100

    feat: add liveSearch functionality, region page merged with home page, customized styles

--- projects/country explorer app/index.js
+++ projects/country explorer app/index.js
@@ -12,35 +12,93 @@ const app = express();
 app.use(express.static(path.join(__dirname, 'public')));
 
 // all global variables
-const PORT = 3001;
+const PORT = 3000;
 const API_URL = "https://restcountries.com/v3.1";
+let cache = null;
+let lastFetch = 0;
+const TTL = 1000 * 60 * 60 * 24;    // 24 hours
 
-// Home route
-app.get('/', (req, res) => {
-    res.render('index.ejs');
+async function getCountries() {     // fetch all countries data & store as cache
+    const now = Date.now();
+
+    if (!cache || (now - lastFetch > TTL)) {
+        const res = await axios.get(`${API_URL}/all?fields=name,capital,population,region,subregion,languages,currencies,flags,borders,cca3`);
+        cache = res.data;
+        lastFetch = now;
+    };
+    return cache;
+};
+
+(async () => {
+    await getCountries();
+})();
+
+function fetchCountryByName(source, name) {     // takes cache as source and name to filter
+    return source.filter(c => c.name.common.toLowerCase().includes(name.toLowerCase()));
+}
+function fetchCountryByRegion(source, region) {
+    return source.filter(c => c.region.toLowerCase() === region.toLowerCase());
+}
+
+// Home route with all countries loaded
+app.get('/', async (req, res) => {
+
+    const { name } = req.query;
+    const { region } = req.query;
+    
+    let results = cache;
+
+    if(name) {
+        // fetch country by name from cache
+        results = fetchCountryByName(cache, name);
+    }
+    if(region) {
+        // fetch countries by region data from cache
+        results = fetchCountryByRegion(cache, region);
+    }
+
+    res.render('index.ejs', { allCountries: results });
+});
+
+
+// auto search with keystroke
+app.get('/search', (req, res) => {
+    const { name, region } = req.query;
+
+    let results = cache;
+
+    if (region) {
+        results = fetchCountryByRegion(results, region);
+    }
+
+    if(name) {
+        results = fetchCountryByName(results, name);
+    } else if (!region && !name) {
+        results = cache;
+    };
+
+    res.json(results);
 });
 
 // Country route
 app.get('/country', async (req, res) => {
-    const name = req.query.name.toLowerCase();
-    console.log(name);  // debug code
+    const { name } = req.query;
 
     try {
-        // fetch main country data
-        const response = await axios.get(`${API_URL}/name/${name}?fullText=true&fields=name,capital,population,region,subregion,languages,currencies,flags,borders,maps`);
-        console.log(response.data[0]);  // debug code
-        const country = response.data[0];
-
-        let neighbors = [];
-        // Fetch neighboring countries (if any)
-        if (country.borders && country.borders.length > 0) {
-            const codes = country.borders.join(",");
-            const borderResponse = await axios.get(
-                `https://restcountries.com/v3.1/alpha?codes=${codes}&fields=name,flags`
-            );
-        neighbors = borderResponse.data;
-        }
+        // fetch main country data from cache
+        const countryData = fetchCountryByName(cache, name);
+        const country = countryData[0];
         
+        let neighbors = [];
+        // fetch neighboring countries from cache
+        if ( country.borders && country.borders.length > 0 ) {
+            const borderCountries = cache.filter(c => country.borders.includes(c.cca3));
+
+            neighbors = borderCountries.map(c => ({
+                name: c.name.common,
+                flags: c.flags
+            }));
+        };
         res.render('country.ejs', { country, neighbors });
     } catch (error) {
         console.log("error: country not found");
@@ -50,27 +108,9 @@ app.get('/country', async (req, res) => {
     }
 });
 
-// Region route
-app.get('/region', async (req, res) => {
-    const { region } = req.query;
-    console.log(region);    // debug code
-    
-    try {
-        const response = await axios.get(`${API_URL}/region/${region}?fields=name,capital,population,flags`);
-        console.log(response.data[0]);  // debug code
-        const countries = response.data;
-        res.render('region.ejs', { region, countries });
-    } catch (error) {
-        console.log("error: region not found");
-        res.status(404).render("error.ejs", {
-            message: "Unable to fetch countries for this region.",
-        });
-    }
-});
-
 // Catch-all route for errors
 app.get('/*splat', (req, res) => {
-    res.send('404 Page not found');  
+    res.status(404).render('error.ejs', { message: '404 Page not found' });  
 });
 
 // Start server
---
+++ projects/country explorer app/public/js/autoSearch.js
@@ -0,0 +1,52 @@
+console.log("Test test con");
+
+const input = document.getElementById("search-field");
+const countryContainer = document.getElementById("country-container");
+const select = document.querySelector('select[name="region"]');
+
+window.addEventListener("DOMContentLoaded", () => {
+  const urlParams = new URLSearchParams(window.location.search);
+  const regionFromURL = urlParams.get('region');
+  if (regionFromURL) select.value = regionFromURL;
+});
+
+async function liveSearch() {
+  const region = select.value; // <-- get current selection
+  const query = input.value.trim();
+
+  console.log("region:", region);
+  console.log("query:", query);
+
+  const res = await fetch(`/search?name=${query}&region=${region}`);
+  const countries = await res.json();
+
+  if (!countries.length) {
+    countryContainer.classList.add("remove-grid");
+    countryContainer.innerHTML = `
+    <p class="no-match">No countries found. Check your spelling or try another region.</p>
+    `;
+  } else {
+    countryContainer.classList.remove("remove-grid");
+    countryContainer.innerHTML = countries.map(country => `
+      <a class="country-card-small" href="/country?name=${country.name.common}">
+        <img 
+          src="${country.flags.svg}" 
+          alt="${country.flags.alt}"
+        />
+        <h3>${country.name.common}</h3>
+        <p><strong>Capital:</strong> ${country.capital ? country.capital[0] : "N/A"}</p>
+        <p><strong>Population:</strong> ${country.population.toLocaleString()}</p>
+      </a>
+    `).join("");
+  };
+};
+
+let timeout;
+
+input.addEventListener("input", () => {
+  clearTimeout(timeout);
+  // debounce (wait until user stops typing)
+  timeout = setTimeout(liveSearch, 0); // 200ms delay
+});
+
+select.addEventListener("change", liveSearch);
